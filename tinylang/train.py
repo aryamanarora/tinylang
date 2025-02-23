12ba514defade84cd16180451f89989595889701
Aryaman Arora <aryaman.arora2020@gmail.com>
2025-02-22 22:53:08 -0800

    fix important structure

--- tinylang/train.py
+++ tinylang/train.py
@@ -59,7 +59,7 @@ def eval_metrics(model: AutoModel, eval_inputs: torch.Tensor, eval_labels: torch
 def train(
     model: AutoModel,
     pcfg: PCFG,
-    eval_dataset: tuple[torch.Tensor, torch.Tensor]=None,
+    eval_dataset: tuple[torch.Tensor, torch.Tensor] = None,
     batch_size: int = 16,
     num_steps: int = 10000,
 ):
@@ -69,12 +69,13 @@ def train(
     optimizer = AdamW(model.parameters(), lr=1e-4)
 
     # load eval dataset
+    eval_inputs, eval_labels = None, None
     if eval_dataset is None:
         eval_inputs, eval_labels = pcfg.sample_n(64)
-        eval_inputs = torch.tensor(eval_inputs, device=model.device, dtype=torch.long)
-        eval_labels = torch.tensor(eval_labels, device=model.device, dtype=torch.long)
     else:
         eval_inputs, eval_labels = eval_dataset
+    eval_inputs = torch.tensor(eval_inputs, device=model.device, dtype=torch.long)
+    eval_labels = torch.tensor(eval_labels, device=model.device, dtype=torch.long)
 
     iterator = tqdm(range(num_steps), desc="Training")
     metrics = defaultdict(list)
@@ -104,9 +105,3 @@ def train(
         iterator.set_postfix(**postfix)
     
     return metrics
-
-
-if __name__ == "__main__":
-    pcfg = PCFG(num_terminals=20, num_nonterminals=10, max_rhs_len=10, max_rules_per_nt=5, max_depth=10, head_position="left")
-    model = init_model(pcfg)
-    train(model, pcfg)
