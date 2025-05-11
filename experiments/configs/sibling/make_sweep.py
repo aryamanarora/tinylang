87448c506be5de2e9f0dcebad97ad590d8e90b7f
neilrathi <neilrathi@gmail.com>
2025-05-11 15:11:59 -0700

    3 layer siblings

--- experiments/configs/sibling/make_sweep.py
+++ experiments/configs/sibling/make_sweep.py
@@ -5,6 +5,7 @@ ablations = {
     "mixer_type": ["attention", "hyena", "base_conv", "h3", "based", "mamba"],
     "n_embd": [16, 32, 64, 128, 256],
     "lr": [3e-5, 1e-4, 3e-4, 1e-3, 3e-3],
+    "n_layer": [2, 3]
 }
 
 with open("template.yaml", "r") as f:
@@ -15,15 +16,23 @@ flops_unit = None
 for mixer_type in ablations["mixer_type"]:
     for n_embd in ablations["n_embd"]:
         for lr in ablations["lr"]:
-            new_config = deepcopy(config)
-            new_config["model"]["config"]["mixer_type"] = mixer_type
-            new_config["model"]["config"]["n_embd"] = n_embd
-            if mixer_type != "attention":
-                new_config["model"]["config"]["pos_emb"] = False
-            if mixer_type == "mamba":
-                new_config["model"]["config"]["state_mixer_type"] = None
-            new_config["training"]["lr"] = lr
-            # save config
-            name = f"{mixer_type}___{n_embd}___{lr:.0e}"
-            with open(f"./{name}.yaml", "w") as f:
-                yaml.dump(new_config, f)
+            for n_layer in ablations["n_layer"]:
+
+                new_config = deepcopy(config)
+                new_config["model"]["config"]["mixer_type"] = mixer_type
+                new_config["model"]["config"]["n_embd"] = n_embd
+                new_config["model"]["config"]["n_layer"] = n_layer
+                
+                if mixer_type != "attention":
+                    new_config["model"]["config"]["pos_emb"] = False
+                if mixer_type == "mamba":
+                    new_config["model"]["config"]["state_mixer_type"] = None
+                
+                new_config["training"]["lr"] = lr
+                # save config
+                if n_layer == 2:
+                    name = f"{mixer_type}___{n_embd}___{lr:.0e}"
+                else:
+                    name = f"{mixer_type}___{n_embd}___{n_layer}___{lr:.0e}"
+                with open(f"./{name}.yaml", "w") as f:
+                    yaml.dump(new_config, f)
