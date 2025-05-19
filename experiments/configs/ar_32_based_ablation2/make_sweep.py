a021e9076836b670d48580f6b81e9f89fb4c3269
aryamanarora <aryaman.arora2020@gmail.com>
2025-05-19 16:30:03 -0700

    final notebooks, figures, configs

---
+++ experiments/configs/ar_32_based_ablation2/make_sweep.py
@@ -0,0 +1,30 @@
+import yaml
+from copy import deepcopy
+
+ablations = {
+    "mixer_type": ["based"],
+    "n_embd": [16, 32, 64, 128, 256],
+    "lr": [3e-5, 1e-4, 3e-4, 1e-3, 3e-3],
+    "d_conv": [1, 2, 4],
+}
+
+with open("template.yaml", "r") as f:
+    config = yaml.safe_load(f)
+
+flops_unit = None
+
+for mixer_type in ablations["mixer_type"]:
+    for n_embd in ablations["n_embd"]:
+        for lr in ablations["lr"]:
+            for d_conv in ablations["d_conv"]:
+                new_config = deepcopy(config)
+                new_config["model"]["config"]["mixer_type"] = mixer_type
+                new_config["model"]["config"]["n_embd"] = n_embd
+                if mixer_type == "mamba":
+                    new_config["model"]["config"]["state_mixer_type"] = None
+                new_config["training"]["lr"] = lr
+                new_config["model"]["config"]["d_conv"] = d_conv
+                # save config
+                name = f"{mixer_type}_{d_conv}___{n_embd}___{lr:.0e}"
+                with open(f"./{name}.yaml", "w") as f:
+                    yaml.dump(new_config, f)
